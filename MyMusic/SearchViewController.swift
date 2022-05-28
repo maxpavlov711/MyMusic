e329fd6906b0154884680b120003794f147e2fe6
Max Pavlov <max.pavlov711@gmail.com>
2022-05-28 17:15:33 +0300

    MainTabBarController

---
+++ MyMusic/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  MyMusic
+//
+//  Created by Max Pavlov on 28.05.22.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by Max Pavlov on 28.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
