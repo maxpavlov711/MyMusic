399537560d1116a223e16fbae83787008f615c80
Max Pavlov <max.pavlov711@gmail.com>
2022-05-28 20:10:28 +0300

    Setup Search UITableViewController

--- MyMusic/MainTabBarController.swift
+++ MyMusic/MainTabBarController.swift
@@ -13,9 +13,9 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        UITabBar.appearance().backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
-        
+
         tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        tabBar.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
         
         viewControllers = [generateViewController(rootViewController: SearchViewController(), image: #imageLiteral(resourceName: "search"), title: "Search"), generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "library"), title: "Library")]
     }
