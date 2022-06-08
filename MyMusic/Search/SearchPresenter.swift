0512cdb2ade417ec3198d58843f6915e439cd100
Max Pavlov <max.pavlov711@gmail.com>
2022-06-08 18:33:38 +0300

    Search Delay Fixing

--- MyMusic/Search/SearchPresenter.swift
+++ MyMusic/Search/SearchPresenter.swift
@@ -18,8 +18,6 @@ class SearchPresenter: SearchPresentationLogic {
     func presentData(response: Search.Model.Response.ResponseType) {
     
       switch response {
-      case .some:
-          print("presenter .some")
       case .presentTracks(let searchResults):
           let cells = searchResults?.results.map({ track in
               cellViewModel(from: track)
@@ -29,6 +27,8 @@ class SearchPresenter: SearchPresentationLogic {
           print("presenter .presentTracks")
           viewController?.displayData(viewModel: Search.Model.ViewModel.ViewModelData.displayTracks(searchViewModel: searchViewModel))
       
+      case .presentFooterView:
+          viewController?.displayData(viewModel: Search.Model.ViewModel.ViewModelData.displayFooterView)
       }
     }
     
