2be592b885d8789e828f7f4022bbf389cd62bd0f
Max Pavlov <max.pavlov711@gmail.com>
2022-06-09 17:02:22 +0300

    TrackDetailView

---
+++ MyMusic/Search/TrackDetailView.swift
@@ -0,0 +1,17 @@
+//
+//  TrackDetailView.swift
+//  MyMusic
+//
+//  Created by Max Pavlov on 9.06.22.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
+}
