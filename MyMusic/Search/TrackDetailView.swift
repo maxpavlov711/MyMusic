23ee71c70ba5917138c3aa3cf4109870cc995fc9
Max Pavlov <max.pavlov711@gmail.com>
2022-06-17 15:06:34 +0300

    Play Tracks with AVPlayer

--- MyMusic/Search/TrackDetailView.swift
+++ MyMusic/Search/TrackDetailView.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SDWebImage
+import AVKit
 
 class TrackDetailView: UIView {
     
@@ -19,6 +21,11 @@ class TrackDetailView: UIView {
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var volumeSlider: UISlider!
     
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,6 +33,23 @@ class TrackDetailView: UIView {
         trackImageView.backgroundColor = .red
     }
     
+    func set(viewModel: SearchViewModel.Cell) {
+        trackTitleLabel.text = viewModel.trackName
+        authorTitleLabel.text = viewModel.artistName
+        playTrack(previewURL: viewModel.previewUrl)
+        let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
+        guard let url = URL(string: string600 ?? "") else { return }
+        trackImageView.sd_setImage(with: url)
+    }
+    
+    private func playTrack(previewURL: String?) {
+        print("Патюсь включить трэк по ссылке: \(previewURL ?? "отсутсвтует")")
+        
+        guard let url = URL(string: previewURL ?? "") else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
     
     @IBAction func handleValueSlider(_ sender: Any) {
     }
@@ -44,5 +68,12 @@ class TrackDetailView: UIView {
     }
     
     @IBAction func playPauseAction(_ sender: Any) {
+        if player.timeControlStatus == .paused {
+            player.play()
+            playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+        } else {
+            player.pause()
+            playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+        }
     }
 }
